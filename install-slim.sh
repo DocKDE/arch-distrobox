6e1850e5a689093e0d3e66679e9ae29068d103f2
Benedikt Flöser <benedikt.floeser@scandio.de>
2024-02-06 20:38:43 +0100

    Replace install with mv in slim installer

--- install-slim.sh
+++ install-slim.sh
@@ -10,5 +10,5 @@ FILENAME="${DIST}.tar.gz"
 
 curl -sLo "${WORKDIR}/${FILENAME}" "${URL}/${FILENAME}"
 tar -xf "${WORKDIR}/${FILENAME}" -C ${WORKDIR}
-install -c -m 0755 "${WORKDIR}/${DIST}/slim" /usr/bin/
-install -c -m 0755 "${WORKDIR}/${DIST}/slim-sensor" /usr/bin/
+mv "${WORKDIR}/${DIST}/slim" /usr/bin/
+mv "${WORKDIR}/${DIST}/slim-sensor" /usr/bin/
